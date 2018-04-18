6629af1714040828d7691ce49e719fe8ca1a9c97
olesia <zvarich.o.m@gmail.com>
2018-04-18 16:38:41 +0300

    add project

---
+++ assets/js/toogleSidebar.js
@@ -0,0 +1,7 @@
+function toggleSidebar(param) {
+    param.classList.toggle('active');
+    document.getElementById("main-page-menu-dropdown").classList.toggle('active');
+    document.getElementById("main-page").classList.toggle('opacity');
+
+
+}
